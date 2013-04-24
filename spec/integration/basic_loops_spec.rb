2d8000bee7bfa7a402e2232c63daed2947dc22ca
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-04-24 07:41:23 -0700

    Test nested loops

--- spec/integration/basic_loops_spec.rb
+++ spec/integration/basic_loops_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
+require 'brainclusterfuck'
 
 describe 'Basic loops' do
   let(:lexer) { Brainclusterfuck::Lexer.new(program) }
   let(:compiler) { Brainclusterfuck::Compiler.new(lexer.tokens) }
   let(:terminal) { Brainclusterfuck::Terminal.new }
-  let(:memory) { Brainclusterfuck::Memory.new(1) }
+  let(:memory) { Brainclusterfuck::Memory.new(10) }
 
   let(:interpreter) do
     Brainclusterfuck::Interpreter.new(
@@ -12,6 +13,33 @@ describe 'Basic loops' do
       terminal: terminal,
       memory: memory
     )
+  end
+
+  describe 'a nested loop' do
+    let(:program) { '++[->++++[->++++<]<]>>.' }
+    let(:expected_cycles) { 88 } # found and confirmed via experimentation :/
+
+    def self.it_correctly_nests_loops
+      it 'correctly nests loops' do
+        interpreter.step(expected_cycles)
+        expect(interpreter).to be_finished
+
+        expect(interpreter.cycles).to eq(expected_cycles)
+        expect(terminal.text).to eq(' ')
+      end
+    end
+
+    context 'without optimizing' do
+      it_correctly_nests_loops
+    end
+
+    context 'with optimizing' do
+      before do
+        compiler.squeeze_operations!
+      end
+
+      it_correctly_nests_loops
+    end
   end
 
   describe 'the decrement macro' do
@@ -50,6 +78,5 @@ describe 'Basic loops' do
 
       it_clears_the_cell
     end
-
   end
 end
